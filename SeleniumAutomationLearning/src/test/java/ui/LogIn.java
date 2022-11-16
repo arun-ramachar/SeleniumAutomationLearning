88519472b03702ea131d2d421fe58cbc7681a9b3
ArunRamachar <arunramachar@gmail.com>
2022-11-16 14:00:43 +0530

    thirdcommit-1

--- SeleniumAutomationLearning/src/test/java/ui/LogIn.java
+++ SeleniumAutomationLearning/src/test/java/ui/LogIn.java
@@ -8,34 +8,31 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LogIn {
-	
-	 public static String browser= "chrome";
-	 
+
+	public static String browser = "chrome";
+
 //	 webdriver is a interface we can create reference for 
 //	 interface we cant create objects for interface
-	 public static WebDriver driver;
+	public static WebDriver driver;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		 if (browser.equals("firefox")) {
-			 WebDriverManager.chromedriver().setup();
-               driver = new FirefoxDriver (); 
-		 } else if(browser.equals("chrome")) {
-		  WebDriverManager.chromedriver().setup();
-               driver = new ChromeDriver();
-		 }
-		 
+		if (browser.equals("firefox")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+
 		driver.get("https://www.saucedemo.com");
-		
-		
+
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
 		driver.findElement(By.id("password")).sendKeys("secret_sauce");
 		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
 
 		driver.close();
-		
-		
 
 	}
 
