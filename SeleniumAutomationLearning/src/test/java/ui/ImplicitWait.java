82ff572109306be59bc684bef661bd44af93b3c6
ArunRamachar <arunramachar@gmail.com>
2022-11-16 16:16:40 +0530

    implicit commit

---
+++ SeleniumAutomationLearning/src/test/java/ui/ImplicitWait.java
@@ -0,0 +1,54 @@
+package ui;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ImplicitWait {
+	public static String browser = "chrome";
+
+//	 webdriver is a interface we can create reference for 
+//	 interface we cant create objects for interface
+	public static WebDriver driver;
+	
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws InterruptedException{
+		// TODO Auto-generated method stub
+		
+
+		
+
+			if (browser.equals("firefox")) {
+				WebDriverManager.chromedriver().setup();
+				driver = new FirefoxDriver();
+			} else if (browser.equals("chrome")) {
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+			}
+			
+			driver.get("https://www.ebay.com.au");
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			//thread .sleep is a hard code not recomendded
+			//bcoz if u say 2000 milliseconds it will wait 2000 sec../
+			//but implicit wait wont wait 2sec it will access fast..only 
+			//its wait if dont get element...depend on sec
+			WebElement element = driver.findElement(By.xpath(" //*[@id=\"mainContent\"]/div[1]/ul/li[3]/a"));
+			
+			Actions action = new Actions(driver);
+			
+			action.moveToElement(element).perform();
+			
+			driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[3]/div[2]/div[1]/nav[1]/ul/li[1]/a")).click();
+			
+			driver.close();
+	}
+
+}
