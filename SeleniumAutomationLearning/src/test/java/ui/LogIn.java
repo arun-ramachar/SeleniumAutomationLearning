45692de2b9d3fedc5ba39e02a2072991b7dd723c
ArunRamachar <arunramachar@gmail.com>
2022-11-16 13:58:50 +0530

    thirdcommit

--- SeleniumAutomationLearning/src/test/java/ui/LogIn.java
+++ SeleniumAutomationLearning/src/test/java/ui/LogIn.java
@@ -1,19 +1,31 @@
 package ui;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LogIn {
+	
+	 public static String browser= "chrome";
+	 
+//	 webdriver is a interface we can create reference for 
+//	 interface we cant create objects for interface
+	 public static WebDriver driver;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		WebDriverManager.chromedriver().setup();
-
-		ChromeDriver driver = new ChromeDriver();
-
+		 if (browser.equals("firefox")) {
+			 WebDriverManager.chromedriver().setup();
+               driver = new FirefoxDriver (); 
+		 } else if(browser.equals("chrome")) {
+		  WebDriverManager.chromedriver().setup();
+               driver = new ChromeDriver();
+		 }
+		 
 		driver.get("https://www.saucedemo.com");
 		
 		
