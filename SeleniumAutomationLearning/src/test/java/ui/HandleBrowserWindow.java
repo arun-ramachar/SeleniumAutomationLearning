3cd7184ebc4a5e25af4bf564741a0c655cc9c5ff
ArunRamachar <arunramachar@gmail.com>
2023-02-06 15:37:53 +0530

    new commit feb

--- SeleniumAutomationLearning/src/test/java/ui/HandleBrowserWindow.java
+++ SeleniumAutomationLearning/src/test/java/ui/HandleBrowserWindow.java
@@ -1,5 +1,11 @@
 package ui;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -21,13 +27,63 @@ public class HandleBrowserWindow {
 		//getWindowHandles..returns the multiple id of the window
 		
 		
-		String windowID=driver.getWindowHandle();//retrun ID of the single browser window
+//		String windowID=driver.getWindowHandle();//Return ID of the single browser window
+//		
+//		System.out.println(windowID);//every time it return different id remember..
+//		
+		 
 		
-		System.out.println(windowID);
+		driver.findElement(By.xpath("//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[3]/div[2]/p[2]/a")).click();
+		
+		Set<String> windowIDs = driver.getWindowHandles();
+		
+	     //first mehod..
+		
+//		 Iterator<String> it=windowIDs.iterator();
+//		
+//		 String parentID = it.next();//it gives first string element
+//		 String childID = it.next();//it gives child element..
+//		 
+//		 System.out.println(parentID);
+//		 System.out.println(childID);
+		 
+      // i couldnot identify the path.. please take care later..
+		 
+		  
+		 //second method using list/ArrrayList..
+		 
+		 //we converted set into list by creating Arraylist..
+		//ArrayList is a class.. list is a interface..
+		  List<String> windowIDList = new ArrayList(windowIDs);
+//		  
+//		String parentWindowId=windowIDList.get(0);
+//		String childWindowId = windowIDList.get(1);
+//		
+//		System.out.println("parentWindowId:" + parentWindowId);
+//		System.out.println("childWindowId:" + childWindowId);
 		
 		
 		
 		
+		//what to do with this ids?
+		//ids mainly used to switch between one browser window to another browser..
+		
+		 //third method:
+		  
+		  for(String windowId:windowIDList) {
+			  
+			  String title =driver.switchTo().window(windowId).getTitle();
+			  System.out.println(title);
+		  }
+		
+		
+		 //difference between close() and quit()..
+		  //close() will close single browsere which is pointing driver..
+		  
+		//quit() will close all browsers...
+		  
+		  
 	}
+	
 
 }
---
+++ SeleniumAutomationLearning/src/test/java/ui/HandleLinks.java
@@ -0,0 +1,100 @@
+package ui;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class HandleLinks {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		
+		 int brokenLink = 0;
+	        
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+        //driver.get("https://www.amazon.in/");
+		
+        driver.get("http://www.deadlinkcity.com/");
+        
+       
+        driver.manage().window().maximize();
+        
+        //1. how to locate links in selenium WebDriver?
+        
+        //driver.findElement(By.linkText("Today's Deals")).click();
+        
+        //driver.findElement(By.partialLinkText("Deals")).click();
+        
+        //succesful output...
+        
+        //2.how to capture all the links from webPage ?
+        
+        //List<WebElement> list=driver.findElements(By.tagName("a"));
+        
+        //System.out.println("number of link present:" + list.size());
+        
+//        for(int i=0;i<list.size();i++) {
+//        	
+//        System.out.println(list.get(i).getText());
+//        System.out.println(list.get(i).getAttribute("href"));
+//        
+//         
+//       }
+        
+//        for(WebElement lists:list) {
+//        	
+//        	System.out.println(lists.getText());
+//         System.out.println(lists.getAttribute("href"));
+//        }
+        
+        
+       // 3.  what is the broken Link? how to check broken lInk?
+        
+        List<WebElement> links=driver.findElements(By.tagName("a"));
+        
+        for(WebElement element: links) {
+        	String url= element.getAttribute("href");
+        	
+        	if(url==null || url.isEmpty()) {
+        		
+        		System.out.println("URL is empty");
+        		
+        		continue;
+        	}
+        	
+          URL link = new URL(url);
+        	try {
+				HttpURLConnection httpconn = (HttpURLConnection) link.openConnection();
+				httpconn.connect();
+				if(httpconn.getResponseCode()>=400) {
+					System.out.println("brokenLink");
+					brokenLink++;
+				}else {
+					System.out.println("validLink");
+				}
+			} 
+        	
+        	catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	
+        }
+        
+	}
+
+}
